ab1825097736f618f0cefcf83b5c7df69c20da5d
James Prestwich <james@prestwi.ch>
2019-10-18 17:31:43 -0700

    chore: cleanup setup

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
+# flake8: noqa
+
 from setuptools import setup, find_packages
+
+reqs = [
+    'ledgerblue',
+    'riemann-tx==2.0.1',
+    'mypy-extensions'
+]
 
 setup(
     name='riemann-ledger',
     version='0.0.3',
-    description=('Sign Bitcoin transactions on your Ledger Nano S'),
+    url='https://github.com/summa-tx/riemann-ledger',
+    description=('Sign Segwit Bitcoin transactions on your Ledger Nano S'),
     author=["James Prestwich"],
-    license="LGPLv3.0",
-    install_requires=[
-        'ledgerblue',
-        'riemann-tx',
-        'mypy-extensions'],
+    author_email='james@summa.one',
+    install_requires=reqs,
     packages=find_packages(),
-    package_data={'ledger': ['py.typed']},
     package_dir={'ledger': 'ledger'},
-    python_requires='>=3.6'
+    package_data={'ledger': ['py.typed']},
+    python_requires='>=3.6',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+    ]
 )
